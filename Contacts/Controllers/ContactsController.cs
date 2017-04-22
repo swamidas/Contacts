71235c678931d1e6186bf8b73fb687084f483e79
sitsgd <sitsgd@AU49060>
2017-04-23 08:29:06 +1000

    Add project files.

---
+++ Contacts/Controllers/ContactsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Contacts.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            List<Contact> model = (List<Contact>) HttpContext.Application["Contacts"];
+            ViewBag.Message = "Set from Index action - Count:" + model.Count;
+            return View(model);
+        }
+
+        // GET: Contacts/Details/5  
+        public ActionResult Details(int id)
+        {
+            Contact model = GetModel(id);
+            return View(model);
+        }
+
+        private Contact GetModel(int? id = null)
+        {
+            var list = (List<Contact>)HttpContext.Application["Contacts"];
+            ViewBag.List = list;
+            var model = (id.HasValue) ? list.First(x => x.Id == id) : new Contact();
+            return model;
+        }
+
+        // GET: Contacts/Create
+        public ActionResult Create()
+        {
+            Contact model = GetModel();
+            return View("Edit", model); //reuse Edit view out of laziness
+        }
+
+        // POST: Contacts/Create
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var list = (List<Contact>)HttpContext.Application["Contacts"];
+                    list.Add(contact);
+                    HttpContext.Application["Contacts"] = list;
+                    return RedirectToAction("Index");
+                }
+                return View(contact);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contacts/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Contact model = GetModel(id);
+            return View(model);
+        }
+
+        // POST: Contacts/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Contact contact)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var list = (List<Contact>)HttpContext.Application["Contacts"];
+                    list.Remove(list.First(x => x.Id == contact.Id));
+                    list.Add(contact);
+                    HttpContext.Application["Contacts"] = list;
+                    return RedirectToAction("Index");
+                }
+                return View(contact);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Contacts/Delete/5
+        public ActionResult DeleteConfirm(int id)
+        {
+            return View();
+        }
+
+        // POST: Contacts/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Count()
+        {
+            ViewBag.Message = ViewBag.Message == null ? "Set in Index action not carried onto Count action" : ViewBag.Message;
+            return PartialView();
+        }
+    }
+}
---
+++ Contacts/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Contacts
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Application["Contacts"] = new List<Contact>()
+            {
+                new Contact(){Id=1, FName="Yuvraj", LName="Dalvi", DOB=new DateTime(2005, 4, 3), Gender=Gender.Male, Hobbies= new List<Hobby>(){Hobby.Cricket, Hobby.Tennis}},
+                new Contact(){Id=2, FName="Laxmi", LName="Dalvi", DOB=new DateTime(1978, 6, 30), Gender=Gender.Female, Hobbies= new List<Hobby>(){Hobby.Cooking, Hobby.Movies}}
+            };
+        }
+    }
+}
